b73e3342c10943df889311597b6507910d6ee5f9
T-Boya <t.osewe1@gmail.com>
2018-02-13 10:38:35 +0300

    J'ai édité le README.md

--- JS/scripts.js
+++ JS/scripts.js
@@ -3,7 +3,7 @@
         return number1 + number2;
       };
 
-      var number1 = parseInt(prompt("Ecrit un nombre:"));
-      var number2 = parseInt(prompt("Ecrit un autre nombre:"));
+      var number1 = parseInt(prompt("Ecris un nombre:"));
+      var number2 = parseInt(prompt("Ecris un autre nombre:"));
       var result = add(number1, number2);
       alert(result);

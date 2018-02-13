e38bbc5673bac6b9a6495126f0c0a3b633c45886
T-Boya <t.osewe1@gmail.com>
2018-02-13 10:12:44 +0300

    J'ai fait une calculatrice en français, pour Moringa

---
+++ JS/scripts.js
@@ -0,0 +1,9 @@
+
+      var add = function(number1, number2) {
+        return number1 + number2;
+      };
+
+      var number1 = parseInt(prompt("Ecrit un nombre:"));
+      var number2 = parseInt(prompt("Ecrit un autre nombre:"));
+      var result = add(number1, number2);
+      alert(result);
